4eceafc59e383f52101793e011f1cef8697c35b9
jacollins01 <jacollins01@P2P5DE43D.admin.bvsd.k12.co.us>
2018-04-30 12:31:14 -0600

    THE MOTOS ARE NOW SONIC '06 SONIC

--- src/org/pumatech/teams/Skynet/Moto.java
+++ src/org/pumatech/teams/Skynet/Moto.java
@@ -1,5 +1,6 @@
 package org.pumatech.teams.Skynet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.pumatech.ctf.*;
@@ -26,34 +27,37 @@ public class Moto extends AbstractPlayer {
 	}
 	
 	public Location avoid(List<Location> scan, Location target) {
-		/*for(Location test : scan) {
+		ArrayList<Location> temp = new ArrayList<Location>(scan);
+		for(Location test : scan) {
 			if(test.getCol() != this.getLocation().getCol() && test.getRow() != this.getLocation().getRow()) {
 				//test for attacker 'auras'
 				List<AbstractPlayer> theirPlayers = this.getTeam().getOpposingTeam().getPlayers();
 				for(AbstractPlayer detect : theirPlayers) {
 					if(this.getGrid().get(test) == detect) {
-						scan.remove(test);
+						temp.remove(test);
 					}
 					for(Actor a : this.getGrid().getNeighbors(test)) {
 						if(a == detect) {
-							scan.remove(test);
+							temp.remove(test);
 						}
 					}
 				}
 			}
-		}*/
+		}
+		scan = temp;
 		
-		// Current issue: ignores both target and returned value from avoid() and goes for (0,0)
+		// Current issue: get last location and don't go back
 		//determine optimal direction
 		int minDir = 360;
 		Location best = scan.get(0);
 		for(Location l : scan) {
 			int a = this.getLocation().getDirectionToward(l);
-			if(Math.abs(this.getLocation().getDirectionToward(target)-a) < minDir) {
+			int t = this.getLocation().getDirectionToward(target);
+			if(Math.abs(t-a) < minDir) {
 				if(getGrid().getEmptyAdjacentLocations(l).size() > 1) {
 					best = l;
 				}
-				minDir = a;
+				minDir = Math.abs(t-a);
 			}
 		}
 		return best;
