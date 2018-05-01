297f7aadaa71381641b982795a9a9abeebad56d9
jeffrey <jeffrey@jeffrey-XPS-8700>
2018-04-30 23:05:03 -0600

    We need to beef up the defense and FIX THE MOTOS' CRUD AGAIN

--- src/org/pumatech/teams/Skynet/T850.java
+++ src/org/pumatech/teams/Skynet/T850.java
@@ -1,7 +1,7 @@
 package org.pumatech.teams.Skynet;
 
+import java.util.List;
 import org.pumatech.ctf.AbstractPlayer;
-
 import info.gridworld.grid.Location;
 
 public class T850 extends AbstractPlayer {
@@ -10,18 +10,48 @@ public class T850 extends AbstractPlayer {
 	// to the flag if there are no more targets given to it by Skynet
 	public T850(Location startLocation) {
 		super(startLocation);
+		this.setTarget(null);
+	}
+	public T850(Location startLocation, Location target) {
+		super(startLocation);
+		this.setTarget(target);
 	}
 
+	private Location pastLocation;
 	public static Location target;
 
-	public void SetTarget(Location l) {
+	public void setTarget(Location l) {
 		target = l;
 	}
 
 	public Location getMoveLocation() {
-		// choose target here
-		Location best = this.getLocation();
-		// calculate optimal route to target here
+		List<Location> possibleMoveLocations = this.getGrid().getEmptyAdjacentLocations(getLocation()); 
+		if(target != null && this.getGrid().get(target) != null) {
+			//path to target
+			return goTowards(possibleMoveLocations, target);
+		}else if(this.getLocation() != this.getTeam().getFlag().getLocation()){
+			//path to flag
+			return goTowards(possibleMoveLocations, this.getTeam().getFlag().getLocation());
+		}else {
+			return this.getLocation();
+		}
+	}
+	
+	public Location goTowards(List<Location> scan, Location target) {
+		// determine optimal direction
+		int minDir = 360;
+		Location best = scan.get(0);
+		for(Location l : scan) {
+			int a = this.getLocation().getDirectionToward(l);
+			int t = this.getLocation().getDirectionToward(target);
+			if(Math.abs(t-a) < minDir) {
+				if(getGrid().getEmptyAdjacentLocations(l).size() > 1 && l != pastLocation) {
+					best = l;
+					pastLocation = this.getLocation();
+				}
+				minDir = Math.abs(t-a);
+			}
+		}
 		return best;
 	}
 }
