e6f11cf4fedfb9796b546221e8b99fc14eb98640
NCI-PC <NCI-PC@DESKTOP-9FUTHM4.Home>
2018-05-01 09:36:11 -0600

    todo: find out how to communicate to specific players

--- src/org/pumatech/teams/Skynet/Skynet.java
+++ src/org/pumatech/teams/Skynet/Skynet.java
@@ -1,5 +1,8 @@
 package org.pumatech.teams.Skynet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.pumatech.ctf.AbstractPlayer;
 
 import info.gridworld.grid.Location;
@@ -9,16 +12,45 @@ public class Skynet extends AbstractPlayer {
 	// attackers
 	public Skynet(Location startLocation) {
 		super(startLocation);
-		
-		
-		
 	}
-	
+
 	public Location getMoveLocation() {
-//		first we need to check in an 8 grid radius of the flag, 
-//		returning first found target for t1ks to go to, wotherwise they will not move
-//		then we check on our side, assigning closest one to T850 to chase, else t850 wont move
-//		processing and updating of target lists goes here
+		// first we need to check in an 8 grid radius of the flag,
+		// returning first found target for t1ks to go to, otherwise they will not move
+		// then we check on our side, assigning closest one to T850 to chase, else t850
+		// wont move
+		// processing and updating of target lists goes here
+		List<AbstractPlayer> a = this.getTeam().getOpposingTeam().getPlayers();
+		int flagc = getTeam().getFlag().getLocation().getCol();
+		int flagr = getTeam().getFlag().getLocation().getRow();
+		int closest[] = new int[7];
+		/*
+		 * creating a array of the distance of enemy players to flags
+		 */
+		for (int i = 0; i < a.size(); i++) {
+			int x = a.get(i).getLocation().getCol();
+			int y = a.get(i).getLocation().getRow();
+			int distance = (int) Math.sqrt(((flagc - x) * (flagc - x)) + ((flagr - y) * (flagr - y)));
+			closest[i] = distance;
+		}
+	
+		//Choosing T1k targets
+		for (int i = 0; i < closest.length; i++) {
+			if (closest[i]<= 8) {
+				// send target to t1k's 
+				break;
+			} else {
+				// send each t1k to an opposite corner of flag 
+			}	
+		}
+		//choosing target for T850
+
+		for (int j = 0; j < closest.length; j++) {
+
+		}
+
 		return this.getLocation();
+
 	}
+
 }
