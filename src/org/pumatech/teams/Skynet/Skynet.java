0ed44b0b6dbb1e1e22dbab9cbb69e4417bbad489
ncirwin01 <ncirwin01@P2P523FCD.admin.bvsd.k12.co.us>
2018-05-02 11:31:26 -0600

    no more out of bounds

--- src/org/pumatech/teams/Skynet/Skynet.java
+++ src/org/pumatech/teams/Skynet/Skynet.java
@@ -15,7 +15,7 @@ public class Skynet extends AbstractPlayer {
 	}
 
 	public Location getMoveLocation() {
-		// first we need to check in an 8 grid radius of the flag,
+		// first we need to check in an 8 grid radius of the flag- actually checking for
 		// returning first found target for t1ks to go to, otherwise they will not move
 		// then we check on our side, assigning closest one to T850 to chase, else t850
 		// wont move
@@ -23,29 +23,30 @@ public class Skynet extends AbstractPlayer {
 		List<AbstractPlayer> a = this.getTeam().getOpposingTeam().getPlayers();
 		int flagc = getTeam().getFlag().getLocation().getCol();
 		int flagr = getTeam().getFlag().getLocation().getRow();
-		int closest[] = new int[7];
-		/*
-		 * creating a array of the distance of enemy players to flags
-		 */
+		ArrayList<Integer> Closest = new ArrayList<Integer>();
+
+		// creating a array of the distance of enemy players to flags
+
 		for (int i = 0; i < a.size(); i++) {
 			int x = a.get(i).getLocation().getCol();
 			int y = a.get(i).getLocation().getRow();
 			int distance = (int) Math.sqrt(((flagc - x) * (flagc - x)) + ((flagr - y) * (flagr - y)));
-			closest[i] = distance;
+			Closest.add(i, distance);
 		}
-	
-		//Choosing T1k targets
-		for (int i = 0; i < closest.length; i++) {
-			if (closest[i]<= 8) {
-				// send target to t1k's 
+
+		// Choosing T1k targets
+		for (int i = 0; i < Closest.size(); i++) {
+			if (Closest.get(i) <= 8) {
+				System.out.println("hi");
+				// send target to t1k's
 				break;
 			} else {
-				// send each t1k to an opposite corner of flag 
-			}	
+				// send each t1k to an opposite corner of flag by giving them an empty thing
+			}
 		}
-		//choosing target for T850
+		// choosing target for T850
 
-		for (int j = 0; j < closest.length; j++) {
+		for (int j = 0; j < Closest.size(); j++) {
 
 		}
 
