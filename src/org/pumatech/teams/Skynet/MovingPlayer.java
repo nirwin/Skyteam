af91857c3850c0c43b97a95d915b4279177a9cc2
jacollins01 <jacollins01@P2P5DE43D.admin.bvsd.k12.co.us>
2018-05-18 12:22:43 -0600

    Pull this one

--- src/org/pumatech/teams/Skynet/MovingPlayer.java
+++ src/org/pumatech/teams/Skynet/MovingPlayer.java
@@ -9,7 +9,7 @@ import info.gridworld.grid.Location;
 public abstract class MovingPlayer extends AbstractPlayer {
 
 	static ArrayList<Location> locationBlacklist = new ArrayList<Location>();
-	static int blacklistSize = 30;
+	static int blacklistSize = 24; // used to be 30
 	
 	public MovingPlayer(Location startLocation) {
 		super(startLocation);
