fb8006e6b8301c551cf229b8b63408e247a0e2c9
jeffrey <jeffrey@jeffrey-XPS-8700>
2018-05-19 18:49:26 -0600

    Added readability to Moto

--- src/org/pumatech/teams/Skynet/Moto.java
+++ src/org/pumatech/teams/Skynet/Moto.java
@@ -10,14 +10,13 @@ import info.gridworld.grid.Location;
 
 /*
 This is Skynet's main offensive player.
-This class is my pride and joy. Break it and I might do bad things to you. 
+This class is my pride and joy. Break it and I will be angry with you. 
 -Jeffrey Collins
 */
 
 public class Moto extends MovingPlayer {
 
 	private Location pastLocation;
-	private ArrayList<Location> stuckOnYou = new ArrayList<Location>();
 
 	public Moto(Location startLocation) {
 		super(startLocation);
@@ -28,32 +27,33 @@ public class Moto extends MovingPlayer {
 		if (possibleMoveLocations.size() == 0) {
 			return null;
 		}
+		// keep the blacklist at a specified size
 		if (locationBlacklist.size() > blacklistSize) {
 			for (int i = 0; i < locationBlacklist.size() - blacklistSize; i++) {
 				locationBlacklist.remove(locationBlacklist.size() - 1);
 			}
 		}
-		if (stuckOnYou.size() > blacklistSize) {
-			for (int i = 0; i < stuckOnYou.size() - blacklistSize; i++) {
-				stuckOnYou.remove(stuckOnYou.size() - 1);
-			}
-		}
+		// return to side if the flag is obtained
 		if (hasFlag()) {
 			return avoid(possibleMoveLocations, this.getTeam().getFlag().getLocation());
 		}
+		// otherwise go towards the flag
 		return avoid(possibleMoveLocations, this.getTeam().getOpposingTeam().getFlag().getLocation());
 	}
 
 	public Location avoid(List<Location> scan, Location target) {
+		// do nothing if there are no empty locations
 		if (scan.size() <= 0) {
 			return this.getLocation();
 		}
+		// prevent concurrent modification exceptions
 		ArrayList<Location> temp = new ArrayList<Location>(scan);
+		// remove unsafe movement options
 		for (Location test : scan) {
 			if (locationBlacklist.contains(test)) {
 				temp.remove(test);
 			}
-			// test for attacker 'auras'
+			// avoid being tagged
 			List<AbstractPlayer> theirPlayers = this.getTeam().getOpposingTeam().getPlayers();
 			for (AbstractPlayer detect : theirPlayers) {
 				if (this.getGrid().get(test) == detect) {
@@ -75,27 +75,30 @@ public class Moto extends MovingPlayer {
 				}
 			}
 		}
+		// update scan
 		scan = temp;
 
+		// do nothing if there are no safe locations
 		if (scan.size() <= 0) {
 			return this.getLocation();
 		}
 
-		// determine optimal direction
+		// determine optimal location based on direction
 		int minDir = 360;
 		Location best = scan.get(0);
-		for (Location l : scan) {
-			int a = this.getLocation().getDirectionToward(l);
-			int t = this.getLocation().getDirectionToward(target);
-			if (Math.abs(t - a) < minDir) {
-				if (this.getGrid().getEmptyAdjacentLocations(l).size() > 1) {
-					if (!locationBlacklist.contains(l) && !l.equals(pastLocation)) {
-						best = l;
+		for (Location loc : scan) {
+			int actual = this.getLocation().getDirectionToward(loc);
+			int optimal = this.getLocation().getDirectionToward(target);
+			if (Math.abs(optimal - actual) < minDir) {
+				if (this.getGrid().getEmptyAdjacentLocations(loc).size() > 1) {
+					if (!locationBlacklist.contains(loc) && !loc.equals(pastLocation)) {
+						best = loc;
 					}
 				}
-				minDir = Math.abs(t - a);
+				minDir = Math.abs(optimal - actual);
 			}
 		}
+		// blacklist unsuitable locations
 		if (Math.abs(
 				this.getLocation().getDirectionToward(best) - this.getLocation().getDirectionToward(target)) >= 90) {
 			if (!locationBlacklist.contains(best)) {
@@ -103,28 +106,47 @@ public class Moto extends MovingPlayer {
 			}
 		}
 		if (best.equals(pastLocation)) {
+			// blacklist to prevent moving back
 			if (!locationBlacklist.contains(best)) {
 				locationBlacklist.add(best);
-				if (!stuckOnYou.contains(best)) {
-					stuckOnYou.add(best);
+			}
+			// take a step back
+			if(target.getCol() > this.getLocation().getCol()) {
+				if(this.getLocation().getCol() > 0) {
+					Location stepBack = new Location(this.getLocation().getCol()-1, this.getLocation().getRow());
+					if(scan.contains(stepBack)) {
+						best = stepBack;
+					}
+				}
+			}
+			if(target.getCol() < this.getLocation().getCol()) {
+				if(this.getLocation().getCol() < this.getGrid().getNumCols()) {
+					Location stepBack = new Location(this.getLocation().getCol()+1, this.getLocation().getRow());
+					if(scan.contains(stepBack)) {
+						best = stepBack;
+					}
 				}
 			}
 		}
+		// update pastLocation and return
 		pastLocation = this.getLocation();
 		return best;
 	}
 
-	public void takeOnMe() {
+	// formerly known as public void takeOnMe()
+	public void printMap() {
 		// print out game map
 		List<Location> occupied = this.getGrid().getOccupiedLocations();
 		for (int row = 0; row < this.getGrid().getNumRows(); row++) {
 			for (int col = 0; col < this.getGrid().getNumCols(); col++) {
 				Location scan = new Location(row, col);
 				if (occupied.contains(scan)) {
-					// occupied space
+					// print actors
 					Actor obj = this.getGrid().get(scan);
 					if (obj instanceof Rock) {
 						System.out.print("#");
+					} else if (obj instanceof Flag) {
+						System.out.print("F");
 					} else if (obj instanceof Moto) {
 						System.out.print("M");
 					} else if (obj instanceof T850) {
@@ -133,15 +155,16 @@ public class Moto extends MovingPlayer {
 						System.out.print("T");
 					} else if (obj instanceof SkynetDupe) {
 						System.out.print("S");
-					} else if (obj instanceof Flag) {
-						System.out.print("F");
 					}
 				} else if (locationBlacklist.contains(scan)) {
+					// print blacklisted Locations
 					System.err.print("#");
 				} else {
+					// print empty
 					System.out.print("-");
 				}
 			}
+			// new line
 			System.out.println();
 		}
 	}
